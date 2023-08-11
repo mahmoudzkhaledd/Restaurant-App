29e11a1f3b1081193eabe6b01adb3fada17dc025
mahmoudzkhaledd <mahmoudnaggar2002@gmail.com>
2023-08-11 10:17:12 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:foodapp/GeneralWidgets/AppText.dart';
+import 'package:foodapp/Shared/Fonts/CairoFont.dart';
+import 'package:foodapp/Shared/Fonts/RalewayFont.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+import 'Features/StartingFeatures/Splash/View/SplashPage.dart';
+
+void main() {
+  AppText.arabicFont = CairoFont.cairoRegular;
+  AppText.englishFont = RalewayFont.ralewayRegular;
+  runApp(const FoodApp());
+}
+
+class FoodApp extends StatelessWidget {
+  const FoodApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      locale: const Locale('ar'),
+      localizationsDelegates: const [
+        GlobalCupertinoLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale("ar"),
+      ],
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 0,
+        ),
+        useMaterial3: true,
+      ),
+      home: const SplashPage(),
+    );
+  }
+}
