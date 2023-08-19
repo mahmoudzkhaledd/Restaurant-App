fdd7f4b8762ccdf3b4ffe0c56b18c863588adfcd
seiffmohamedd <seif500mohamed@gmail.com>
2023-08-19 15:10:04 +0300

    admin only now can add restaurants , migration working , bl working , logo save in config direc successfully

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -16,34 +16,40 @@ class RestaurantController extends Controller
         return view('restaurant.create');
     }
 
-    public function store(Request $request){
+    
+    public function store(Request $request)
+    {
         // Validate the image upload
-    $request->validate([
-        'logo' => 'image|mimes:jpeg,png,jpg,gif|max:2048', // Example validation for image file
-    ]);
-
-    // Handle the image upload
-    if ($request->hasFile('logo')) {
-        $logoPath = $request->file('logo')->store('restaurant_logos', 'public');
-
-        // Save the file path in the 'logo' column
-        // $restaurant->logo = $logoPath;
+        $request->validate([
+            'logo' => 'image|mimes:jpeg,png,jpg,gif|max:2048', // Example validation for image file
+            // Add validation rules for other form fields here
+        ]);
+    
+        // Handle the image upload
+        if ($request->hasFile('logo')) {
+            $logoPath = $request->file('logo')->store('restaurant_logos', 'public');
+    
+            // Create a new restaurant record with the file path in the 'logo' column
+            $restaurant = new Restaurant();
+            $restaurant->logo = $logoPath;
+    
+            // Set other restaurant data from the form
+            $restaurant->name = $request->input('name');
+            $restaurant->location = $request->input('location');
+            $restaurant->description = $request->input('description');
+            $restaurant->rating = $request->input('rating');
+            $restaurant->contact_details = $request->input('contact_details');
+            $restaurant->cuisine_type = $request->input('cuisine_type');
+            $restaurant->email = $request->input('email');
+            $restaurant->password = bcrypt($request->input('password'));
+    
+            // Save the restaurant record to the database
+            $restaurant->save();
+        }
+    
+        // Redirect or return a response to indicate successful creation
     }
-
-    // Save other restaurant data and store it in the database
-    // $restaurant->name = $request->input('name');
-    // $restaurant->location = $request->input('location');
-    // $restaurant->description = $request->input('description');
-    // $restaurant->rating = $request->input('rating');
-    // $restaurant->contact_details = $request->input('contact_details');
-    // $restaurant->cuisine_type = $request->input('cuisine_type');
-    // $restaurant->email = $request->input('email');
-    // $restaurant->password = bcrypt($request->input('password'));
-
-    // // Save the restaurant record
-    // $restaurant->save();
-
-    }
+    
 
     public function edit(){
         //
--- resources/views/restaurant/create.blade.php
+++ resources/views/restaurant/create.blade.php
@@ -13,55 +13,55 @@
 
                         <!-- Restaurant Name -->
                         <div class="form-group">
-                            <label for="name">Restaurant Name</label>
+                            <label for="name" style="color:red">Restaurant Name</label>
                             <input type="text" name="name" id="name" class="form-control" required>
                         </div>
 
                         <!-- Location -->
                         <div class="form-group">
-                            <label for="location">Location</label>
+                            <label for="location" style="color:red">Location</label>
                             <textarea name="location" id="location" class="form-control" required></textarea>
                         </div>
 
                         <!-- Logo -->
                         <div class="form-group">
-                            <label for="logo">Restaurant Logo</label>
-                            <input type="text" name="logo" id="logo" class="form-control-file" accept="image/*" required>
+                            <label for="logo" style="color:red">Restaurant Logo</label>
+                            <input type="file" name="logo" id="logo" class="form-control-file" accept="image/*" required>
                         </div>
 
                         <!-- Description -->
                         <div class="form-group">
-                            <label for="description">Description</label>
+                            <label for="description" style="color:red">Description</label>
                             <textarea name="description" id="description" class="form-control" required></textarea>
                         </div>
 
                         <!-- Rating -->
                         <div class="form-group">
-                            <label for="rating">Rating</label>
+                            <label for="rating" style="color:red">Rating</label>
                             <input type="text" name="rating" id="rating" class="form-control" required>
                         </div>
 
                         <!-- Contact Details -->
                         <div class="form-group">
-                            <label for="contact_details">Contact Details</label>
+                            <label for="contact_details" style="color:red">Contact Details</label>
                             <input type="text" name="contact_details" id="contact_details" class="form-control" required>
                         </div>
 
                         <!-- Cuisine Type -->
                         <div class="form-group">
-                            <label for="cuisine_type">Cuisine Type</label>
+                            <label for="cuisine_type" style="color:red">Cuisine Type</label>
                             <input type="text" name="cuisine_type" id="cuisine_type" class="form-control" required>
                         </div>
 
                         <!-- Email -->
                         <div class="form-group">
-                            <label for="email">Email</label>
+                            <label for="email" style="color:red">Email</label>
                             <input type="email" name="email" id="email" class="form-control" required>
                         </div>
 
                         <!-- Password -->
                         <div class="form-group">
-                            <label for="password">Password</label>
+                            <label for="password" style="color:red">Password</label>
                             <input type="password" name="password" id="password" class="form-control" required>
                         </div>
 
@@ -69,7 +69,7 @@
                         <input type="hidden" name="remember_token" value="{{ Str::random(60) }}">
 
                         <div class="form-group">
-                            <button type="submit" class="btn btn-primary">Create Restaurant</button>
+                            <button type="submit" style="color:red" class="btn btn-primary">Create Restaurant</button>
                         </div>
                     </form>
                 </div>
