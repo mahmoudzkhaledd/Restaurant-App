0c7e5aff781fe7feb1066e2d8df0fe5b7d989a98
mahmoudzkhaledd <mahmoudnaggar2002@gmail.com>
2023-08-22 11:20:24 +0300

    const values

--- lib/Features/Auth/EmailVerification/View/Widgets/EmailVerificationBody.dart
+++ lib/Features/Auth/EmailVerification/View/Widgets/EmailVerificationBody.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:foodapp/GeneralWidgets/CustomButton.dart';
 import 'package:foodapp/Shared/Fonts/CairoFont.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
 import '../../../../../GeneralWidgets/AppText.dart';
 
 class EmailVerificationBody extends StatelessWidget {
-  TextEditingController pinController = TextEditingController();
+  EmailVerificationBody({super.key});
+  final TextEditingController pinController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Column(
         children: [
           Expanded(
@@ -25,19 +25,18 @@ class EmailVerificationBody extends StatelessWidget {
                         "assets/images/email_verification.png",
                         height: MediaQuery.of(context).size.height / 2.5,
                       ),
-
-                      AppText(
+                      const AppText(
                         "تأكيد الإيميل",
                         style: TextStyle(
-                         fontFamily: CairoFont.cairoBold,
-                         fontSize: 26,
+                          fontFamily: CairoFont.cairoBold,
+                          fontSize: 26,
                         ),
                         textAlign: TextAlign.center,
                       ),
-
-                      SizedBox(height: 10,),
-
-                      AppText(
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const AppText(
                         "من فضلك، ادخل الرمز المكون من ٦ أرقام المرسلة إلى الإيميل الذي ادخلته",
                         textAlign: TextAlign.center,
                         style: TextStyle(
@@ -46,34 +45,32 @@ class EmailVerificationBody extends StatelessWidget {
                           fontSize: 15,
                         ),
                       ),
-
-                      SizedBox(height: 20,),
-
+                      const SizedBox(
+                        height: 20,
+                      ),
                       PinCodeTextField(
                         appContext: context,
                         controller: pinController,
                         length: 6,
                         keyboardType: TextInputType.number,
                         pinTheme: PinTheme(
-                          activeColor: Color.fromRGBO(44, 85, 125, 1),
+                          activeColor: const Color.fromRGBO(44, 85, 125, 1),
                           shape: PinCodeFieldShape.box,
                           borderRadius: BorderRadius.circular(7),
                           inactiveColor: Colors.grey,
-                          selectedColor: Color.fromRGBO(44, 85, 125, 1),
+                          selectedColor: const Color.fromRGBO(44, 85, 125, 1),
                         ),
                       ),
-
-
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          AppText("لم تستلم الرمز؟"),
-
-                          SizedBox(width: 10,),
-
+                          const AppText("لم تستلم الرمز؟"),
+                          const SizedBox(
+                            width: 10,
+                          ),
                           TextButton(
-                              onPressed: (){},
-                              child: AppText("اعد الإرسال"),
+                            onPressed: () {},
+                            child: const AppText("اعد الإرسال"),
                           ),
                         ],
                       ),
@@ -83,15 +80,18 @@ class EmailVerificationBody extends StatelessWidget {
               ),
             ),
           ),
-
           CustomButton(
-            onTap: (){},
+            onTap: () {},
             text: "تأكيد",
             verticalPadding: 20,
-            icon: Icon(Icons.check_circle, color: Colors.white,),
+            icon: const Icon(
+              Icons.check_circle,
+              color: Colors.white,
+            ),
           ),
-
-          SizedBox(height: 20,),
+          const SizedBox(
+            height: 20,
+          ),
         ],
       ),
     );
--- lib/Features/Auth/ForgotPassword/View/Widgets/ForgotPasswordBody.dart
+++ lib/Features/Auth/ForgotPassword/View/Widgets/ForgotPasswordBody.dart
@@ -7,12 +7,13 @@ import 'package:foodapp/Shared/Fonts/CairoFont.dart';
 import 'package:get/get.dart';
 
 class ForgotPasswordBody extends StatelessWidget {
-  TextEditingController emailController = TextEditingController();
+  ForgotPasswordBody({super.key});
+  final TextEditingController emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
       child: GestureDetector(
         onTap: () => FocusManager.instance.primaryFocus!.unfocus(),
         child: Column(
@@ -22,50 +23,51 @@ class ForgotPasswordBody extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Image.asset(
-                      "assets/images/forgot_password.png"
-                    ),
-
-                    AppText(
+                    Image.asset("assets/images/forgot_password.png"),
+                    const AppText(
                       "نسيت كلمة السر؟",
                       style: TextStyle(
                         fontFamily: CairoFont.cairoBold,
                         fontSize: 26,
                       ),
                     ),
-
-                    SizedBox(height: 10,),
-
-                    AppText(
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const AppText(
                       "لا تقلق! من فضلك ادخل الإيميل الخاص بحسابك.",
                       style: TextStyle(
                         color: Colors.grey,
                       ),
                     ),
-
-                    SizedBox(height: 20,),
-
+                    const SizedBox(
+                      height: 20,
+                    ),
                     CustomTextBox(
                       hintText: "الإيميل",
                       controller: emailController,
                     ),
-
-                    SizedBox(height: 20,),
+                    const SizedBox(
+                      height: 20,
+                    ),
                   ],
                 ),
               ),
             ),
-
             CustomButton(
               onTap: () {
                 Get.to(() => PasswordResetPage());
               },
               text: "التالي",
               verticalPadding: 20,
-              icon: Icon(Icons.navigate_next , color: Colors.white,),
+              icon: const Icon(
+                Icons.navigate_next,
+                color: Colors.white,
+              ),
             ),
-
-            SizedBox(height: 20,),
+            const SizedBox(
+              height: 20,
+            ),
           ],
         ),
       ),
