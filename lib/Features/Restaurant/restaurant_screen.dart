81340da7c42c18257f846f8e7f2467283f073987
Sherifwaelnagaty <Sherifwael0@gmail.com>
2023-08-28 22:56:23 +0300

    Finishing OrdersScreen

---
+++ lib/Features/Restaurant/restaurant_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class RestaurantScreen extends StatefulWidget {
+  const RestaurantScreen({super.key});
+
+  @override
+  State<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends State<RestaurantScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
